e732ab9e670657b66e3bd15a31449de812ec0ba6
Bernardo Semião <berna.1991.1@gmail.com>
2026-05-25 21:01:54 +0200

    installed dependancies and scafolded project

---
+++ src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import LoginPage from '@/pages/login/loginPage.vue';
+
+</script>
+
+<template>
+  <div class="flex min-w-screen min-h-screen items-center justify-center ">
+    <LoginPage />
+  </div>
+</template>
